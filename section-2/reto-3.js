0b790ce1756d6cf9de0b05e3f9f81fdbf4e20786
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-04-23 21:42:04 -0500

    Add duplicate product functionalitie in reto-3.js in section 2

--- section-2/reto-3.js
+++ section-2/reto-3.js
@@ -1,9 +1,9 @@
 //Section 2 Reto 3
 let products =[];
-
-function validateAlphabet(validator){
-    const validateAlphabet = /^[a-zA-ZñÑáéíóúüÁÉÍÓÚÜ]+$/;
-    return validateAlphabet.test(validator);
+let id=0;
+function validateAlphabetNumeric(validator){
+    const validateAlphabetNumeric = /^[a-zA-ZñÑáéíóúüÁÉÍÓÚÜ]+[\s\d]*$/;
+    return validateAlphabetNumeric.test(validator);
 }
 function validateNumber(validator){
     const validateNumber = /^\d+$/;
@@ -11,26 +11,25 @@ function validateNumber(validator){
 }
 function createProducts() {
     let name, price, amount, description, product;
-        while(true){
-            name = prompt("Ingresa el nombre del producto");
-            if(validateAlphabet(name) && name){
-                price = prompt("Ingresa el precio del producto");
-            }
-            if(validateNumber(price) && price){
-                amount = prompt("Ingresa la cantidad");
-                price=parseFloat(price);
-            }
-            if(validateNumber(amount) && amount){
-                description = prompt("Ingresa una descripción");
-                amount=parseFloat(amount);
-            }
-            if(description){
-                break
-            }else{
-                alert("Valor inválido");
-            }
-        }
+    do{
+        name = prompt("Ingresa el nombre del producto");
+        name = name.toLowerCase().trim();
+    }while(!(validateAlphabetNumeric(name) && name))
+    do {
+        price = prompt("Ingresa el precio del producto");
+        price = parseFloat(price);
+    } while (!(validateNumber(price) && price));
+    do {
+        amount = prompt("Ingresa la cantidad");
+        amount = parseFloat(amount);
+    } while (!(validateNumber(amount) && amount));
+    do {
+        description = prompt("Ingresa una descripción");
+        description = description.trim();
+    } while (!(description));
+    id++;
     product={
+        id,
         name,
         price,
         amount,
@@ -40,5 +39,35 @@ function createProducts() {
     alert("Producto agregado con éxito");
     return product;
 }
+function duplicateProducts(){
+    let product, duplicate, originalProduct,counter;
+    do{
+        product = prompt("Ingrese el nombre del producto que desea")
+        product = product.toLowerCase().trim();
+    }while(!(validateAlphabetNumeric(product) && product));
+    counter=2;
+    originalProduct = products.find(e=> e.name === product);
+    if (originalProduct){
+        id++
+        duplicate = {
+            id,
+            name: originalProduct.name+" Copy",
+            price: originalProduct.price,
+            amount: originalProduct.amount,
+            description: originalProduct.description
+        }
+        while(duplicate.name in products.find(e=> e.name === product)){
+            duplicate.name = originalProduct.name+" Copy "+counter;
+            counter++;
+        }
+        products.push(duplicate)
+    }else{
+        alert("El producto ingresado no existe")
+    }
+
+}
 createProducts()
+duplicateProducts()
+duplicateProducts()
+duplicateProducts()
 console.log(products);
